23dc659bc6b89a2ea2a63d8309334482ecc15c90
Евгений <zholudev.evgeniy@mail.ru>
2021-12-07 00:29:28 +0300

    commit 2

---
+++ src/Student.java
@@ -0,0 +1,79 @@
+public class Student {
+
+    private String id;
+    private String firstName;
+    private String secondName;
+    private int age;
+    private String university;
+    private int course;
+    private String group;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                ", university='" + university + '\'' +
+                ", course=" + course +
+                ", group='" + group + '\'' +
+                '}';
+    }
+}
--- src/XMLParser.java
+++ src/XMLParser.java
@@ -11,21 +11,33 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+/*
+Так, короче, смотри
+Этот комент потом стереть не забудь)
+Я тебе тут накинул парсер в строку, в мапу и в список сущностей.
+Если нужен какой-то один, то удали остальные. Друг на друге они не завязаны.
+Большую часть я написал комментарии, если что не понятно - говори.
+* */
+
+
 public class XMLParser {
 
     public static void main(String[] args) {
         String xmlString = parseXMLToString("src/students.xml");
         System.out.println(xmlString);
 
-        List<Map<String, String>> xmlList = parseXmlToList("src/students.xml");
+        Map<String, Map<String, String>> xmlMap = parseXmlToMap("src/students.xml");
+        System.out.println(xmlMap);
+
+        List<Student> xmlList = parseXmlToList("src/students.xml");
         System.out.println(xmlList);
     }
 
+    // Метод парсит xml в строку
     private static String parseXMLToString(String path) {
         // Получаем DOM документ по пути к файлу xml
         Document doc = getDocument(path);
-        // Приводим xml документ к нормальному виду
-        doc.getDocumentElement().normalize();
         // Получаем список нод с тэгом "Student"
         NodeList students = doc.getElementsByTagName("Student");
         StringBuilder result = new StringBuilder();
@@ -55,6 +67,7 @@ public class XMLParser {
 
     }
 
+    // Метод возвращает DOM документ по пути к файлу
     private static Document getDocument(String path) {
         try {
             // Создаем объект, который будет представлять собой xml файл
@@ -62,21 +75,61 @@ public class XMLParser {
             // Создается построитель документа
             DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             // Создается дерево DOM документа из файла
-            return documentBuilder.parse(file);
+            Document result = documentBuilder.parse(file);
+            // Приводим xml документ к нормальному виду
+            result.getDocumentElement().normalize();
+            return result;
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return null;
     }
 
-    private static List< Map<String, String>> parseXmlToList(String path) {
+    // Метод парсит xml в список
+    private static List<Student> parseXmlToList(String path) {
         // Получаем DOM документ по пути к файлу xml
         Document doc = getDocument(path);
-        // Приводим xml документ к нормальному виду
-        doc.getDocumentElement().normalize();
         // Получаем список нод с тэгом "Student"
         NodeList students = doc.getElementsByTagName("Student");
-        List< Map<String, String>> result = new ArrayList<>();
+        List<Student> result = new ArrayList<>();
+        for (int i = 0; i < students.getLength(); i++) {
+            Node studentNode = students.item(i);
+            if (studentNode.getNodeType() == Node.ELEMENT_NODE) {
+                // Если нода является элементом, приводим её к типу Element
+                Element studentElement = (Element) studentNode;
+                Student student = new Student();
+                // Получаем аттрибут id и добавляем его в мапу свойств
+                student.setId(studentElement.getAttribute("id"));
+                // Получаем свойства и проставляем их в сущность Student
+                student.setFirstName(getStringProperty(studentElement, "FirstName"));
+                student.setSecondName(getStringProperty(studentElement, "SecondName"));
+                student.setAge(getIntProperty(studentElement, "Age"));
+                student.setUniversity(getStringProperty(studentElement, "University"));
+                student.setCourse(getIntProperty(studentElement, "Course"));
+                student.setGroup(getStringProperty(studentElement, "Group"));
+                // Добавляем в список студентов нового с его свойствами
+                result.add(student);
+            }
+        }
+        return result;
+    }
+
+    // Метод возвращает строковое значение свойства студента
+    private static String getStringProperty(Element studentElement, String propertyName){
+        return studentElement.getElementsByTagName(propertyName).item(0).getChildNodes().item(0).getNodeValue();
+    }
+    // Метод возвращает числовое значение свойства студента
+    private static int getIntProperty(Element studentElement, String propertyName){
+        return Integer.parseInt(getStringProperty(studentElement, propertyName));
+    }
+
+    // Метод парсит xml в мапу
+    private static Map<String, Map<String, String>> parseXmlToMap(String path) {
+        // Получаем DOM документ по пути к файлу xml
+        Document doc = getDocument(path);
+        // Получаем список нод с тэгом "Student"
+        NodeList students = doc.getElementsByTagName("Student");
+        Map<String, Map<String, String>> result = new HashMap<>();
         for (int i = 0; i < students.getLength(); i++) {
             Node studentNode = students.item(i);
             if (studentNode.getNodeType() == Node.ELEMENT_NODE) {
@@ -84,8 +137,8 @@ public class XMLParser {
                 Element studentElement = (Element) studentNode;
                 // Создаем мапу, которая будет хранить свойства
                 Map<String, String> properties = new HashMap<>();
-                // Получаем аттрибут id и добавляем его в мапу свойств
-                properties.put("id", studentElement.getAttribute("id"));
+                // Получаем аттрибут id
+                String id = studentElement.getAttribute("id");
                 // Получаем список нод, находящихся в ноде Student
                 NodeList studentProperties = studentElement.getChildNodes();
                 for (int j = 0; j < studentProperties.getLength(); j++) {
@@ -95,11 +148,11 @@ public class XMLParser {
                         properties.put(studentProperty.getNodeName(), studentProperty.getChildNodes().item(0).getTextContent());
                     }
                 }
-                // Добавляем в список студентов нового с его свойствами
-                result.add(properties);
+                // Добавляем в мапу свойства студента по его id
+                result.put(id, properties);
             }
         }
         return result;
-
     }
+
 }
